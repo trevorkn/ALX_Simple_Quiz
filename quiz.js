3933464aed3964890461e0951d002a49755cf5c3
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-06-19 17:14:09 +0300

    add a functional simple quiz with answer validation and feedback

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function checkAnswer() {
+   const correctAnswer = "4";
+  const selectedOption = document.querySelector('input[name="quiz"]:checked');
+  if (selectedOption) {
+    const userAnswer = selectedOption.value;
+    if (userAnswer === correctAnswer){
+        document.getElementById("feedback").textContent = "Correct! Well done";
+    }
+    else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+    }
+  }
+  else {
+    document.getElementById("feedback").textContent = "Please select an answer.";
+  }
+}
+
+//Event Listener
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
